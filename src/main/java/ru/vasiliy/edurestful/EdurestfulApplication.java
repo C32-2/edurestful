4075d6a751c60a37bc6ee6d061c3a176fa09eafd
C32-2 <vasilybichutsky@gmail.com>
2025-04-13 10:05:47 +0300

    Initial commit

---
+++ src/main/java/ru/vasiliy/edurestful/EdurestfulApplication.java
@@ -0,0 +1,13 @@
+package ru.vasiliy.edurestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vasiliy/edurestful/EdurestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasiliy.edurestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
